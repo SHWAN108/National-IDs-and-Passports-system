b9379e52d770f2c5e30b9201f2aa5d9237645503
PayamRasho <payamrfattah01@gmail.com>
2022-10-16 00:01:42 +0300

    adding class to employee

---
+++ src/SystemForNationalIdAndPassport/Employee.java
@@ -0,0 +1,9 @@
+package SystemForNationalIdAndPassport;
+
+import java.util.*;
+
+public class Employee {
+    public Employee(String firstName, String lastName, int age, int id, int password, String email, Date dob,
+            Gender gender, Position position) {
+}
+}
