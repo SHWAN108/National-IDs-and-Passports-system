dbb0ce20305dd1d2acfa52e957cf8c9a82492bbd
Ramel1212 <95249398+Ramel1212@users.noreply.github.com>
2022-10-16 00:23:34 +0300

    adding citizens.java

--- src/SystemForNationalIdAndPassport/Citizens.java
+++ src/SystemForNationalIdAndPassport/Citizens.java
@@ -1,1 +1,10 @@
+package SystemForNationalIdAndPassport;
 
+import java.util.*;
+
+public class Citizens extends User {
+    public Citizens(String firstName, String lastName, int age, int id, int password, String email, Date dob,
+            Gender gender, Position position) {
+        super();
+    }
+}
