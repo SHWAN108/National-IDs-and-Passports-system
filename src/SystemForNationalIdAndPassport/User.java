b460bad9d4b573dcba71ba58a0d2b142e0571d76
unknown <you@example.com>
2022-10-15 23:42:17 +0300

    adding user variable

--- src/SystemForNationalIdAndPassport/User.java
+++ src/SystemForNationalIdAndPassport/User.java
@@ -1,1 +1,15 @@
 package SystemForNationalIdAndPassport;
+
+import java.util.*;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private int id;
+    private int password;
+    private String email;
+    private Date dob;
+    private Gender gender;
+    private Position position;
+    }
