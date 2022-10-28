c69c65a5ba2401f5f949ddc542216ddbb5cae08d
SHWAN108 <shwanshero888@gmail.com>
2022-10-28 17:29:29 +0300

    small change

--- src/SystemForNationalIdAndPassport/model/Administrator.java
+++ src/SystemForNationalIdAndPassport/model/Administrator.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Administrator extends User {
 
     public Administrator(String firstName, String lastName, int age, int id, int password, String email, Date dob,
-            Gender gender, Position position) {
+            Gender gender, Position position, BlType bloodType) {
         super();
     }
 }
