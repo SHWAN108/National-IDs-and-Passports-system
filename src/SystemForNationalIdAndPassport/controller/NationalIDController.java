551cb99a0adf3b1dcf7f46674e98ca3525ccfae8
SHWAN108 <shwanshero888@gmail.com>
2022-10-30 05:08:38 +0300

    adding nationalid and passport controller

--- src/SystemForNationalIdAndPassport/controller/NationalIDController.java
+++ src/SystemForNationalIdAndPassport/controller/NationalIDController.java
@@ -1,57 +1,60 @@
 package SystemForNationalIdAndPassport.controller;
 
+import SystemForNationalIdAndPassport.model.BlType;
+import SystemForNationalIdAndPassport.model.Gender;
 import SystemForNationalIdAndPassport.model.NationalID;
 import SystemForNationalIdAndPassport.model.NationalIDApprovingStatus;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class NationalIDController {
     public static ArrayList<NationalID> approvedNationalId = new ArrayList<>();
     public static ArrayList<NationalIDApprovingStatus> nonApprovedNationalId = new ArrayList<>();
 
-    public boolean create(){
+    public boolean create(String firstName, String lastName, int nationalId, Date dob,
+            Gender gender, BlType bloodType, Date expDate) {
+        NationalID shwan = new NationalID(firstName, lastName, nationalId, dob, gender, bloodType, expDate);
+        approvedNationalId.add(shwan);
         return false;
     }
 
-    public boolean update()
-    {
+    public boolean update(String firstName, Date expDate, int nationalId) {
+
         return false;
     }
 
-    public boolean renew(){
+    public ArrayList<NationalID> showAllNationalId() {
+        approvedNationalId.stream().forEach(System.out::println);
+        return approvedNationalId;
+    }
+
+    public NationalID getNationalId(int nationalId) {
+        return getNationalId(nationalId);
+    }
+
+    public ArrayList<NationalIDApprovingStatus> showAllPendingNationalId() {
+        approvedNationalId.stream().forEach(System.out::println);
+        return nonApprovedNationalId;
+
+    }
+
+    public ArrayList<NationalIDApprovingStatus> showAllRejectedNationalId() {
+        approvedNationalId.stream().forEach(System.out::println);
+        return nonApprovedNationalId;
+    }
+
+    public ArrayList<NationalID> showAllApprovedNationalId() {
+        approvedNationalId.stream().forEach(System.out::println);
+        return approvedNationalId;
+    }
+
+    public boolean leadAllDataToList() {
         return false;
     }
 
-    public ArrayList<String> showAllNationalId(){
-        return null;
-    }
-
-    public NationalID getNationalId(int nationalId){
-        return null;
-    }
-
-    public ArrayList<String> showAllPendingNationalId(){
-        return null;
-    }
-
-    public ArrayList<String> showAllRejectedNationalId(){
-        return null;
-    }
-
-    public ArrayList<String> showAllApprovedNationalId(){
-        return null;
-    }
-
-
-
-    public boolean leadAllDataToList(){
+    public boolean saveListToFile() {
         return false;
     }
-
-    public boolean saveListToFile(){
-        return false;
-    }
-
-
 
 }
--- src/SystemForNationalIdAndPassport/controller/PassportController.java
+++ src/SystemForNationalIdAndPassport/controller/PassportController.java
@@ -1,54 +1,61 @@
 package SystemForNationalIdAndPassport.controller;
 
+import SystemForNationalIdAndPassport.model.BlType;
+import SystemForNationalIdAndPassport.model.Gender;
 import SystemForNationalIdAndPassport.model.NationalID;
 import SystemForNationalIdAndPassport.model.Passport;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class PassportController {
     public static ArrayList<Passport> approvedPassport = new ArrayList<>();
     public static ArrayList<Passport> nonApprovedPassport = new ArrayList<>();
 
-    public boolean create(){
+    public boolean create(String firstName, String lastName, int id, Date dob,
+            Gender gender, BlType bloodType, Date expDate) {
+        Passport shwan = new Passport(firstName, lastName, id, dob, gender, bloodType, expDate);
+        approvedPassport.add(shwan);
         return false;
     }
 
-    public boolean update()
-    {
+    public boolean renew() {
         return false;
     }
 
-    public boolean renew(){
+    public ArrayList<Passport> showAllPassport() {
+        approvedPassport.stream().forEach(System.out::println);
+        return approvedPassport;
+
+    }
+
+    public NationalID getPassportByPassportID(int nationalId) {
+        return null;
+    }
+
+    public ArrayList<Passport> showAllPendingPassport() {
+        approvedPassport.stream().forEach(System.out::println);
+        return nonApprovedPassport;
+
+    }
+
+    public ArrayList<Passport> showAllRejectedPassport() {
+        nonApprovedPassport.stream().forEach(System.out::println);
+        return nonApprovedPassport;
+
+    }
+
+    public ArrayList<Passport> showAllApprovedPassport() {
+        approvedPassport.stream().forEach(System.out::println);
+        return approvedPassport;
+
+    }
+
+    public boolean leadAllDataToList() {
         return false;
     }
 
-    public ArrayList<String> showAllPassport(){
-        return null;
-    }
-
-    public NationalID getPassportByPassportID(int nationalId){
-        return null;
-    }
-
-    public ArrayList<String> showAllPendingPassport(){
-        return null;
-    }
-
-    public ArrayList<String> showAllRejectedPassport(){
-        return null;
-    }
-
-    public ArrayList<String> showAllApprovedPassport(){
-        return null;
-    }
-
-
-
-    public boolean leadAllDataToList(){
-        return false;
-    }
-
-    public boolean saveListToFile(){
+    public boolean saveListToFile() {
         return false;
     }
 }
